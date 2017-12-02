b086b962af8b5c600b83c36737ed75c3283abfd2
Alex <aliptsey@gmail.com>
2017-12-01 17:51:50 -0800

    Update with generate dish function

--- bot.js
+++ bot.js
@@ -6,18 +6,30 @@ var Bot = new TwitterBot({
  access_token: process.env.BOT_ACCESS_TOKEN,
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = [ "hey twitter",
-                    "im tweeting",
-                    "tweet tweet",
-                    "tweetstorm time... 1/22",
-                    "plz RT v important",
-                    "delete ur account",
-                    "it me",
-                    "same",
-                    "#dogpants go on 4 legs!!",
-                    "#thedress is blue and black" ];
-function chooseRandom(myArray) {
-  return myArray[Math.floor(Math.random() * myArray.length)];
-}
-var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+ function generateDish() {
+      var ADJ_PROB = 0.5;
+      var STRUCTURE_PROB = 0.5;
+    var adjs = ["fried", "unlimited", "bottomless", "cheesy", "sloppy", "white cheddar", "Angus", "low-fat", "pumpkin cream", "All-American", "fit", "Santa Fe", "supreme", "sizzling", "bourbon", "buffalo", "cajun", "cali", "crispy", "ultimate", "deep dish", "homestyle", "classic", "southwest", "meat-lovers", "extra", "glazed", "garden", "harvest cheddar", "jalepeno", "sour cream and onion", "ranch", "tangy", "zesty", "cool ranch", "queso", "all beef", "$8", "$4", "barbecue ranch", "almond-crusted"];
+      var ingredients = ["bacon", "chicken", "cheese", "beef", "havarti", "fish", "white chocolate", "pork", "cinnamon roll", "pepperoni", "ham", "roast beef", "crayfish", "clam", "calamari"];
+      var structures = ["patty", "melt", "patty melt", "pie", "pancake", "pizza", "hot dog", "fingers", "rings", "slurpee", "sundae", "burger", "nuggets", "skins", "sliders", "sampler", "platter", "steak", "doughnut", "waffle", "smores", "nachos", "poppers", "wedges", "bites", "scrambler", "meal", "chowder", "slam", "bowl", "combo", "sandwich"];
+      var dish = "";
+      if (_.random(0, 100) < ADJ_PROB * 100) {
+        dish += _.sample(adjs) + " ";
+      }
+      dish += _.sample(ingredients) + " ";
+      if (_.random(0, 100) < STRUCTURE_PROB * 100) {
+        dish += _.sample(structures) + " ";
+      }
+      dish += "with ";
+      if (_.random(0, 100) < ADJ_PROB * 100) {
+        dish += _.sample(adjs) + " ";
+      }
+      dish += _.sample(ingredients) + " ";
+      if (_.random(0, 100) < STRUCTURE_PROB * 100) {
+        dish += _.sample(structures) + " ";
+      }
+      return dish;
+      }
+
+var dish = generateDish(); 
+Bot.tweet(dish);
